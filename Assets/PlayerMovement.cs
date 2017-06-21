6ed77f7d3e6d8cc717e40e2c90f34677678a26e8
Andrew Scott <adscott1982@googlemail.com>
2017-06-21 22:22:05 +0100

    Added sprites in scene for rocket and thrusters.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float acceleration = 5f;
+    private float speed = 0f;
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private float GetThumbstickDirection(string axis)
+    {
+        direction = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = rotation;
+    }
+}
