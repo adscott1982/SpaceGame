a51b157197c2a8280c3e9f45c7599ce345d76f69
Andrew Scott <adscott1982@googlemail.com>
2017-06-21 23:05:39 +0100

    Added logic to read thumbstick direction from controller and apply rotation to game object.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -14,13 +14,25 @@ public class PlayerMovement : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        var leftThumbstick = GetThumbstick();
+        //var direction = Vector2.Angle(Vector2.up, leftThumbstick);
+        var direction = GetDirectionFromThumbStickVector2(leftThumbstick);
 
-    private float GetThumbstickDirection(string axis)
+        if (direction.HasValue)
+        {
+            Debug.Log("X = " + leftThumbstick.x + "Y = " + leftThumbstick.y + " Direction = " + direction);
+            this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, direction.Value));
+        }
+    }
+
+    private Vector2 GetThumbstick()
     {
-        direction = Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        rotation = Quaternion.LookRotation(direction, Vector3.up);
-        transform.rotation = rotation;
+        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    private float? GetDirectionFromThumbStickVector2(Vector2 vector)
+    {
+        if (vector.magnitude < 0.5f) return null;
+        return (Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg) - 90f;
     }
 }
