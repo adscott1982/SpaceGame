f4bba1db42965ffe78b2f2539026554735a51bff
Andrew Scott <adscott1982@googlemail.com>
2017-06-22 22:50:31 +0100

    Added movement for ship, using left thumbstick. Added code to rotate the ship to the direction of movement, but lerped (damped), but also within a max rotation speed.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,8 +2,10 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float acceleration = 5f;
-    private float speed = 0f;
+    public float maxAcceleration = 0.1f;
+    public float maxRotationSpeed = 0.1f;
+
+    private Vector2 speed = Vector2.zero;
     
 	// Use this for initialization
 	void Start ()
@@ -15,24 +17,54 @@ public class PlayerMovement : MonoBehaviour
 	void Update ()
     {
         var leftThumbstick = GetThumbstick();
-        //var direction = Vector2.Angle(Vector2.up, leftThumbstick);
         var direction = GetDirectionFromThumbStickVector2(leftThumbstick);
-
-        if (direction.HasValue)
-        {
-            Debug.Log("X = " + leftThumbstick.x + "Y = " + leftThumbstick.y + " Direction = " + direction);
-            this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, direction.Value));
-        }
+        this.SetSpeed(leftThumbstick);
+        this.SetRotation(this.speed);
+        this.UpdatePosition();
     }
 
     private Vector2 GetThumbstick()
     {
-        return new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        var vector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        return vector; 
+    }
+
+    private void SetSpeed(Vector2 inputVector)
+    {
+        var acceleration = inputVector * this.maxAcceleration * Time.deltaTime;
+        this.speed += acceleration;
+    }
+
+    private void UpdatePosition()
+    {
+        this.transform.position = this.transform.position.AddVector2(this.speed);
     }
 
     private float? GetDirectionFromThumbStickVector2(Vector2 vector)
     {
         if (vector.magnitude < 0.5f) return null;
         return (Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg) - 90f;
+    }
+
+    private void SetRotation(float direction)
+    {
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, direction));
+    }
+
+    private void SetRotation(Vector2 vector)
+    {
+        var targetRotation = (Mathf.Atan2(vector.y, vector.x) * Mathf.Rad2Deg);
+        var currentRotation = this.transform.rotation.eulerAngles.z;
+        var maxRotationThisFrame = this.maxRotationSpeed * Time.deltaTime;
+
+        var lerpedRotation = Mathf.LerpAngle(currentRotation, targetRotation, 1f * Time.deltaTime);
+        var rotationDelta = lerpedRotation - currentRotation;
+
+        if (Mathf.Abs(rotationDelta) > maxRotationThisFrame)
+        {
+            rotationDelta = Mathf.Sign(rotationDelta) * maxRotationThisFrame;
+        }
+
+        this.transform.rotation = Quaternion.Euler(new Vector3(0, 0, currentRotation + rotationDelta));
     }
 }
---
+++ Assets/Scripts/Extensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class Extensions
+{ 
+    public static Vector3 AddVector2(this Vector3 vector3, Vector2 vector2)
+    {
+        return new Vector3(vector3.x + vector2.x, vector3.y + vector2.y);
+    }
+}
