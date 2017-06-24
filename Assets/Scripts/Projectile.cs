d1ac9ae434c58193e72e3c8666c7cd1944aaecc5
Andrew Scott <adscott1982@googlemail.com>
2017-06-24 22:58:32 +0100

    Added projectiles that fire when using the turret.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float LifeTime = 5f;
+    public float Speed = 2f;
+
+    private Vector2 speedVector;
+
+	// Use this for initialization
+	void Start ()
+    {
+        this.transform.SetParent(null, true);
+        this.speedVector = this.transform.rotation.eulerAngles.z.DegreeToVector2() * this.Speed;  
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        this.DeductTimeFromLife();
+
+        if (this.LifeTime < 0)
+        {
+            Destroy(this.gameObject);
+        }
+
+        this.UpdatePosition();
+	}
+
+    private void UpdatePosition()
+    {
+        this.transform.position = this.transform.position.AddVector2(this.speedVector * Time.deltaTime);
+    }
+
+    private void DeductTimeFromLife()
+    {
+        this.LifeTime -= Time.deltaTime;
+    }
+}
--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    public GameObject ProjectilePrefab;
+    public float FireInterval = 0.5f;
+
     private float thumbstickActivationMag = 0.7f;
+    private float timeToFire = 0f;
 
 	// Use this for initialization
 	void Start ()
@@ -13,22 +18,39 @@ public class Turret : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        this.UpdateTimeToFire();
+
         var rightThumbstick = InputManager.GetRightThumbstick();
 
         if (rightThumbstick.magnitude > this.thumbstickActivationMag)
         {
             this.RotateToDirection(rightThumbstick.Direction());
-            this.FireWeapon();
+
+            if (this.CanFire())
+            {
+                this.FireWeapon();
+            }
         }
 	}
+
+    private bool CanFire()
+    {
+        return this.timeToFire <= 0f;
+    }
 
     private void RotateToDirection(float direction)
     {
         this.transform.rotation = direction.AsEulerZ();
     }
 
+    private void UpdateTimeToFire()
+    {
+        this.timeToFire = this.timeToFire <= 0f ? 0f : this.timeToFire - Time.deltaTime;
+    }
+
     private void FireWeapon()
     {
-
+        this.timeToFire = this.FireInterval;
+        var projectile = Instantiate(this.ProjectilePrefab, this.transform);
     }
 }
